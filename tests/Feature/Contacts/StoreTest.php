97d1d0584db523d693ce0e8ed4b2da09a928d467
sajo13 <sajosunny007@gmail.com>
2025-02-10 16:57:03 +0530

    Add Pest faker package, also add custom exception
    - Pest faker package allow to generate dummy data.
    - Custom exception 'tobePhoneNumber'.
    - Need to start with '+', min 6 character length and numeric type.

--- tests/Feature/Contacts/StoreTest.php
+++ tests/Feature/Contacts/StoreTest.php
@@ -5,7 +5,6 @@ use App\Models\User;
 use App\Models\Contact; // Assuming you have a Contact model
 use Illuminate\Support\Facades\Session;
 
-uses(WithFaker::class);
 
 it("store test", function () {
 
@@ -14,15 +13,15 @@ it("store test", function () {
     $contactData = [
         'account_id' => 1,
         'organization_id' => 1,
-        'first_name' => $this->faker->firstName,
-        'last_name' => $this->faker->lastName,
-        'email' => $this->faker->email,
-        'phone' => $this->faker->e164PhoneNumber,
+        'first_name' => \Pest\Faker\fake()->firstName,
+        'last_name' => \Pest\Faker\fake()->lastName,
+        'email' => \Pest\Faker\fake()->email,
+        'phone' => \Pest\Faker\fake()->e164PhoneNumber,
         'address' => '1 Test Street',
         'city' => 'Testerfield',
-        'region' => 'Debbyshire',
-        'country' => $this->faker->randomElement(['US', 'CA']),
-        'postal_code' => $this->faker->postcode,
+        'region' => 'Derbyshire',
+        'country' => \Pest\Faker\fake()->randomElement(['US', 'CA']),
+        'postal_code' => \Pest\Faker\fake()->postcode,
     ];
 
     //create a record
@@ -35,6 +34,11 @@ it("store test", function () {
     'address' => $contactData['address'],
     ]);
 
-    //assert that the email passed, same as saved.
-    $this->assertSame($contactData['email'], $contact->email);
+    expect(Contact::latest()->first())
+        ->first_name->toBeString()->not->toBeEmpty()
+        ->last_name->toBeString()->not->toBeEmpty()
+        ->email->toBeString()->toContain('@')
+        ->phone->toBePhoneNumber()
+        ->region->toBe('Derbyshire')
+        ->country->toBeIn(['US', 'CA']);
 });
--- tests/Pest.php
+++ tests/Pest.php
@@ -11,6 +11,9 @@
 |
 */
 
+use App\Models\User;
+use PHPUnit\Framework\ExpectationFailedException;
+
 pest()->extend(Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
@@ -26,8 +29,16 @@ pest()->extend(Tests\TestCase::class)
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
+expect()->extend('toBePhoneNumber', function () {
+    expect($this->value)->toBeString()->toStartWith('+');
+
+    if (strlen($this->value) < 6) {
+        throw new ExpectationFailedException('Phone numbers must be at least 6 characters.');
+    }
+
+    if (! is_numeric(Str::of($this->value)->after('+')->remove([' ', '-'])->__toString())) {
+        throw new ExpectationFailedException('Phone numbers must be numeric.');
+    }
 });
 
 /*
