832ac09268babdb1dac5823c163a71c84d56232f
sajo13 <sajosunny007@gmail.com>
2025-02-10 13:49:06 +0530

    Add Contacts Tests
    - Initialize data in beforeEach hook for test setup.
    - Add test cases for viewing and searching contacts, ensuring logic is correctly implemented.

---
+++ tests/Feature/PestContactsTest.php
@@ -0,0 +1,97 @@
+<?php
+
+use App\Models\Account;
+use App\Models\User;
+use Inertia\Testing\AssertableInertia as Assert;
+
+//uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create([
+        'account_id' => Account::create(['name' => 'Acme Corporation'])->id,
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'email' => 'johndoe@example.com',
+        'owner' => true,
+    ]);
+
+    $organization = $this->user->account->organizations()->create(['name' => 'Example Organization Inc.']);
+
+
+    $this->user->account->contacts()->createMany([
+        [
+            'organization_id' => $organization->id,
+            'first_name' => 'Martin',
+            'last_name' => 'Abbott',
+            'email' => 'martin.abbott@example.com',
+            'phone' => '555-111-2222',
+            'address' => '330 Glenda Shore',
+            'city' => 'Murphyland',
+            'region' => 'Tennessee',
+            'country' => 'US',
+            'postal_code' => '57851',
+        ], [
+            'organization_id' => $organization->id,
+            'first_name' => 'Lynn',
+            'last_name' => 'Kub',
+            'email' => 'lynn.kub@example.com',
+            'phone' => '555-333-4444',
+            'address' => '199 Connelly Turnpike',
+            'city' => 'Woodstock',
+            'region' => 'Colorado',
+            'country' => 'US',
+            'postal_code' => '11623',
+        ],
+    ]);
+//    dd(\App\Models\Contact::find(1)->get());
+});
+
+test('can view contacts', function () {
+    $this->actingAs($this->user)
+        ->get('/contacts')
+        ->assertInertia(fn (Assert $assert) => $assert
+            ->component('Contacts/Index')
+            ->has('contacts.data', 2)
+            ->has('contacts.data.0', fn (Assert $assert) => $assert
+                ->where('id', 1)
+                ->where('name', 'Martin Abbott')
+                ->where('phone', '555-111-2222')
+                ->where('city', 'Murphyland')
+                ->where('deleted_at', null)
+                ->has('organization', fn (Assert $assert) => $assert
+                    ->where('name', 'Example Organization Inc.')
+                )
+            )
+            ->has('contacts.data.1', fn (Assert $assert) => $assert
+                ->where('id', 2)
+                ->where('name', 'Lynn Kub')
+                ->where('phone', '555-333-4444')
+                ->where('city', 'Woodstock')
+                ->where('deleted_at', null)
+                ->has('organization', fn (Assert $assert) => $assert
+                    ->where('name', 'Example Organization Inc.')
+                )
+            )
+        );
+});
+
+test('can search for contacts', function () {
+    $this->actingAs($this->user)
+        ->get('/contacts?search=Martin')
+        ->assertInertia(fn (Assert $assert) => $assert
+            ->component('Contacts/Index')
+            ->where('filters.search', 'Martin')
+            ->has('contacts.data', 1)
+            ->has('contacts.data.0', fn (Assert $assert) => $assert
+                ->where('id', 3)
+                ->where('name', 'Martin Abbott')
+                ->where('phone', '555-111-2222')
+                ->where('city', 'Murphyland')
+                ->where('deleted_at', null)
+                ->has('organization', fn (Assert $assert) => $assert
+                    ->where('name', 'Example Organization Inc.')
+                )
+            )
+        );
+});
+
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use LazilyRefreshDatabase;
+
     protected function setUp(): void
     {
+
         parent::setUp();
 
         $this->withoutVite();
